138246c8936791afe67f890797bcaef7b84418ed
LeonardoZanol <leozianni@outlook.com>
2025-06-08 14:33:55 -0300

    feat: adicionado leitura do arquivo de usuarios

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
+const fs = require('fs')
 
 const app = express()
 const port = 3000
 
 app.use(cors())
+
+function readUsersFile() {
+    const filePath = path.join(__dirname, 'data', 'usuarios.json')
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ erro: 'Erro ao Ler o Arquivo de Usuários.' })
+        }
+
+        res.json(JSON.parse(data))
+
+    })
+}
 
 app.get('/usuarios', (req, res) => {
     
